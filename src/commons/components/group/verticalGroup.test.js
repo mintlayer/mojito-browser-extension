7a02d002a951e82ee288e70931e0943d148eab99
Renie Siqueira <renie.webdev@gmail.com>
2022-05-23 17:03:50 +0300

    feat: adding a bigGap property to verticalGroup - tests for the new property - fixing tests names

--- src/commons/components/group/verticalGroup.test.js
+++ src/commons/components/group/verticalGroup.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import VerticalGroup from './verticalGroup'
 
-test('Render Line component', () => {
+test('Render VerticalGroup component', () => {
   render(<VerticalGroup />)
   const vGroupComponent = screen.getByTestId('vertical-group-container')
 
@@ -9,10 +9,24 @@ test('Render Line component', () => {
   expect(vGroupComponent).toBeEmptyDOMElement()
 })
 
-test('Render Line component with children', () => {
+test('Render VerticalGroup component with children', () => {
   render(<VerticalGroup>content</VerticalGroup>)
   const vGroupComponent = screen.getByTestId('vertical-group-container')
 
   expect(vGroupComponent).toBeInTheDocument()
   expect(vGroupComponent).not.toBeEmptyDOMElement()
+})
+
+test('Render VerticalGroup component - bigGap', async () => {
+  const { rerender } = render(<VerticalGroup/>)
+  let vGroupComponent = screen.getByTestId('vertical-group-container')
+
+  expect(vGroupComponent).toBeInTheDocument()
+  expect(vGroupComponent).not.toHaveClass('bigGap')
+
+  rerender(<VerticalGroup bigGap />)
+  vGroupComponent = screen.getByTestId('vertical-group-container')
+  await waitFor(() => {
+    expect(vGroupComponent).toHaveClass('bigGap')
+  })
 })
