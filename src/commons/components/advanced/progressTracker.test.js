01f0dbc92e872039cb97726ae5fc25f28ade3b1c
Renie Siqueira <renie.webdev@gmail.com>
2022-05-26 14:24:54 +0300

    feat: new component for progress tracking using new component on a screen tests for the new component

---
+++ src/commons/components/advanced/progressTracker.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import ProgressTracker from './progressTracker'
+
+test('Render ProgressTracker component', () => {
+  render(<ProgressTracker />)
+  const progressTrackerComponent = screen.getByTestId('progress-tracker-container')
+
+  expect(progressTrackerComponent).toBeInTheDocument()
+  expect(progressTrackerComponent).not.toBeEmptyDOMElement()
+})
+
+test('Render ProgressTracker component - with steps', () => {
+  const steps = [
+    { name: 'Step 1' },
+    { name: 'Step 2', active: true  },
+    { name: 'Step 3' },
+    { name: 'Step 4' },
+    { name: 'Step 5' },
+  ]
+
+  render(<ProgressTracker steps={steps} />)
+  const progressTrackerComponent = screen.getByTestId('progress-tracker-container')
+  const items = screen.getAllByTestId('progress-step')
+
+  expect(progressTrackerComponent).toBeInTheDocument()
+  expect(items).toHaveLength(items.length)
+})
--- src/pages/set-account-name/index.js
+++ src/pages/set-account-name/index.js
@@ -1,14 +1,24 @@
 import { Link } from 'react-router-dom'
+import ProgressTracker from '../../commons/components/advanced/progressTracker'
 import Input from '../../commons/components/basic/input'
 
 import './setAccountName.css'
 
-const SetAccountName = () => (
-  <div data-testid="set-account-name">
-    <h1 className="center-text">Set Account Name</h1>
-    <Input />
-    <Link to="/" className="back-button"> &lsaquo; </Link>
-  </div>
-)
+const SetAccountName = () => {
+  const steps = [
+    { name: 'Account Name' },
+    { name: 'Setting Password', active: true  },
+    { name: 'Restoring Information' },
+  ]
+
+  return (
+    <div data-testid="set-account-name">
+      <h1 className="center-text">Set Account Name</h1>
+      <ProgressTracker steps={steps}/>
+      <Input />
+      <Link to="/" className="back-button"> &lsaquo; </Link>
+    </div>
+  )
+}
 
 export default SetAccountName
