6fbcf0fe60999bd7ef377f1b170ebf51a7f1e65a
Renie Siqueira <renie.webdev@gmail.com>
2022-06-09 11:48:22 +0300

    feat: Electron API abstractions and tests (#11)

---
+++ src/commons/api/electrum.js
@@ -0,0 +1,39 @@
+const ELECTRUM_URL = 'http://51.158.172.176:3001'
+
+const ELECTRUM_ENDPOINTS = {
+  GET_LAST_BLOCK_HASH: '/blocks/tip/hash',
+  GET_TRANSACTION_DATA: '/tx/:txid',
+  GET_TRANSACTION_STATUS: '/tx/:txid/status',
+  GET_ADDRESS_TRANSACTIONS: '/address/:address/txs',
+  GET_ADDRESS: '/address/:address'
+}
+
+const requestElectrum = async (endpoint, data = null, request = fetch) => {
+  try {
+    const result = await request(ELECTRUM_URL+endpoint)
+    if (!result.ok) throw new Error('Request not successful')
+    const content = await result.text()
+    return Promise.resolve(content)
+  } catch (error) {
+    console.dir(error)
+    throw error
+  }
+}
+
+const getLastBlockHash = () => requestElectrum(ELECTRUM_ENDPOINTS.GET_LAST_BLOCK_HASH)
+const getTransactionData = (txid) => requestElectrum(ELECTRUM_ENDPOINTS.GET_TRANSACTION_DATA.replace(':txid', txid))
+const getTransactionStatus = (txid) => requestElectrum(ELECTRUM_ENDPOINTS.GET_TRANSACTION_STATUS.replace(':txid', txid))
+const getAddressTransactions = (address) => requestElectrum(ELECTRUM_ENDPOINTS.GET_ADDRESS_TRANSACTIONS.replace(':address', address))
+const getAddress = (address) => requestElectrum(ELECTRUM_ENDPOINTS.GET_ADDRESS.replace(':address', address))
+
+
+export {
+  getLastBlockHash,
+  getTransactionData,
+  getTransactionStatus,
+  getAddressTransactions,
+  getAddress,
+  requestElectrum,
+  ELECTRUM_ENDPOINTS,
+  ELECTRUM_URL
+}
