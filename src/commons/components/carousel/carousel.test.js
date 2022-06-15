a59576ab689601c3b0e33bb4d0c52bc68b2203ad
Javier Cabrera <cabrera.javier@gmail.com>
2022-06-15 11:23:18 +0200

    A-1202413006710199: add carousel to do selectables accounts (#13)
    * feat: add carousel to selectable accounts
    
    * fix: enhanced carousel component and test
    
    * fix: comment in carousel test
    
    * fix: removed the import jsdom

---
+++ src/commons/components/carousel/carousel.test.js
@@ -0,0 +1,68 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import Carousel from './carousel'
+
+const data = {
+  accounts: [
+    { id: '1', name: 'Account Name' },
+    { id: '2', name: 'Account Name 2' },
+    { id: '3', name: 'Account Name 3' },
+    { id: '4', name: 'Account Name 4' },
+  ],
+  onClick: jest.fn(),
+  onPrevious: jest.fn(),
+  onNext: jest.fn(),
+}
+
+test('Render Empty Carousel', () => {
+  render(<Carousel />)
+  expect(screen.getByRole('button', { name: 'back' })).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: 'next' })).toBeInTheDocument()
+
+  expect(screen.queryAllByRole('button', { name: 'account' })).toEqual([])
+})
+
+test('Render Carousel', () => {
+  render(<Carousel {...data} />)
+  expect(screen.getByRole('button', { name: 'back' })).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: 'next' })).toBeInTheDocument()
+
+  data.accounts.forEach((account) => {
+    expect(screen.getByText(account.name)).toBeInTheDocument()
+  })
+})
+
+test('Render Carousel onClick', () => {
+  render(<Carousel {...data} />)
+
+  fireEvent.click(screen.getByText('Account Name'))
+  expect(data.onClick).toHaveBeenCalled()
+  screen.getByText('Account Name')
+  expect(screen.getByText('Account Name')).toHaveClass('selected')
+})
+
+test('Render Carousel previousSlide', () => {
+  render(<Carousel {...data} />)
+
+  const backButton = screen.getByRole('button', { name: 'back' })
+  const nextButton = screen.getByRole('button', { name: 'next' })
+
+  fireEvent.click(backButton)
+  expect(data.onPrevious).not.toHaveBeenCalled()
+
+  fireEvent.click(nextButton)
+  fireEvent.click(backButton)
+
+  expect(data.onPrevious).toHaveBeenCalled()
+})
+
+test('Render Carousel nextSlide', () => {
+  render(<Carousel {...data} />)
+  const nextButton = screen.getByRole('button', { name: 'next' })
+
+  for (let i = 0; i < data.accounts.length; i++) {
+    fireEvent.click(nextButton)
+  }
+
+  expect(data.onNext).toHaveBeenCalledTimes(data.accounts.length - 1)
+  expect(nextButton).toBeDisabled()
+})
