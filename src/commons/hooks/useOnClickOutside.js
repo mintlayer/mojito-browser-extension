6f0de16f79cd828ebe785258e4d725a55493acff
Renie Siqueira <renie.webdev@gmail.com>
2022-07-01 16:33:47 +0300

    chore: fixing tests

--- src/commons/hooks/useOnClickOutside.js
+++ src/commons/hooks/useOnClickOutside.js
@@ -1,28 +1,26 @@
-import { useEffect } from 'react'
-/* istanbul ignore next */
+import { useEffect, useRef } from 'react'
+
 export const useOnClickOutside = (ref, handler) => {
-  useEffect(
-    () => {
-      const listener = (event) => {
-        // Do nothing if clicking ref's element or descendent elements
-        if (!ref.current || ref.current.contains(event.target)) {
-          return
-        }
-        handler(event)
-      }
-      document.addEventListener('mousedown', listener)
-      document.addEventListener('touchstart', listener)
-      return () => {
-        document.removeEventListener('mousedown', listener)
-        document.removeEventListener('touchstart', listener)
-      }
-    },
-    // Add ref and handler to effect dependencies
-    // It's worth noting that because passed in handler is a new ...
-    // ... function on every render that will cause this effect ...
-    // ... callback/cleanup to run every render. It's not a big deal ...
-    // ... but to optimize you can wrap handler in useCallback before ...
-    // ... passing it into this hook.
-    [ref, handler],
-  )
+  const effectCalled = useRef(false)
+  const isPartOfModalWindow = (event) =>
+    !ref.current || ref.current.contains(event.target)
+
+  useEffect(() => {
+    /* istanbul ignore next */
+    if (effectCalled.current) return
+    effectCalled.current = true
+
+    const listener = (event) => {
+      if (isPartOfModalWindow(event)) return
+      handler(event)
+    }
+
+    document.addEventListener('mousedown', listener)
+    document.addEventListener('touchstart', listener)
+
+    return () => {
+      document.removeEventListener('mousedown', listener)
+      document.removeEventListener('touchstart', listener)
+    }
+  }, [])
 }
