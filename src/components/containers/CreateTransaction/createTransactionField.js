f70fcfe010a73c877bcfde31a07d9192661cc318
owlsua <allatsnow@gmail.com>
2022-07-18 13:06:01 +0200

    resolve conflicts 02

--- src/components/containers/CreateTransaction/createTransactionField.js
+++ src/components/containers/CreateTransaction/createTransactionField.js
@@ -1,22 +1,100 @@
+import React, { useState } from 'react'
 import { Button, Input } from '@BasicComponents'
 import './createTransactionField.css'
 import { ReactComponent as ArrowIcon } from '@Assets/images/icon-arrow.svg'
 
-const CreateTransactionFiled = ({
+const CreateTransactionField = ({
   label,
-  buttonTitle,
   placeholder,
-  buttonClickHandler,
-  withIcon,
-  bottomText,
-  value,
-  onChengeHandle,
+  buttonTitle,
+  transactionData,
+  inputValue,
+  setInputValue,
 }) => {
+  const [bottomValue, setBottomValue] = useState('')
+  const [currentValueType, setCurrentValueType] = useState(
+    transactionData ? transactionData.tokenName : 'Token',
+  )
+
+  if (!transactionData) {
+    return null
+  }
+
   const buttonExtraClasses = ['create-trasaction-button']
   const inputExtraClasses = ['create-trasaction-input']
+  const fiatName = transactionData.fiatName
+  const tokenName = transactionData.tokenName
+  const exchangeRate = transactionData.exchangeRate
+  const maxCryptoValue = transactionData.maxValueInToken
+  const maxFiatValue = maxCryptoValue * transactionData.exchangeRate
+  /* istanbul ignore next */
+  const finaBottomValue = `≈ ${bottomValue ? bottomValue : 0} ${
+    currentValueType === fiatName ? tokenName : fiatName
+  }`
+  /* istanbul ignore next */
+  const calculateFiatValue = (value) => {
+    return value * exchangeRate
+  }
+
+  /* istanbul ignore next */
+  const calculateCryptoValue = (value) => {
+    return value / exchangeRate
+  }
+
+  /* istanbul ignore next */
+  const updateValue = (value) => {
+    currentValueType === fiatName
+      ? setBottomValue(calculateCryptoValue(value))
+      : setBottomValue(calculateFiatValue(value))
+  }
+
+  /* istanbul ignore next */
+  const switchToFiat = (value) => {
+    setCurrentValueType(fiatName)
+    setBottomValue(value)
+    setInputValue(value > 0 ? calculateFiatValue(value) : '')
+  }
+
+  /* istanbul ignore next */
+  const switchToCrypto = (value) => {
+    setCurrentValueType(tokenName)
+    setBottomValue(value)
+    setInputValue(value > 0 ? calculateCryptoValue(value) : '')
+  }
+
+  /* istanbul ignore next */
+  const changeButtonClickHandler = () => {
+    currentValueType === fiatName
+      ? switchToCrypto(inputValue)
+      : switchToFiat(inputValue)
+  }
+
+  /* istanbul ignore next */
+  const actionButtonClickHandler = () => {
+    if (currentValueType === fiatName) {
+      setInputValue(maxFiatValue)
+      setBottomValue(maxFiatValue / exchangeRate)
+    } else {
+      setInputValue(maxCryptoValue)
+      setBottomValue(maxCryptoValue * exchangeRate)
+    }
+  }
+
+  /* istanbul ignore next */
+  const changeHandler = (e) => {
+    setInputValue(e.target.value)
+    if (currentValueType === tokenName && e.target.value > maxCryptoValue) {
+      setInputValue(maxCryptoValue)
+    }
+    if (currentValueType === fiatName && e.target.value > maxFiatValue) {
+      setInputValue(maxFiatValue)
+    }
+    updateValue(e.target.value)
+  }
+
   return (
     <div
-      className={`create-tr-field ${bottomText && 'create-tr-field-bottom'}`}
+      className="create-tr-field"
       data-testid="create-transaction-field"
     >
       <label
@@ -31,43 +109,43 @@ const CreateTransactionFiled = ({
           id="transactionInput"
           extraStyleClasses={inputExtraClasses}
           placeholder={placeholder}
-          value={value}
-          onChange={onChengeHandle}
+          value={inputValue}
+          onChangeHandle={changeHandler}
+          number
         />
-        {withIcon && (
-          <div
-            className="create-tr-icons-wrapper"
-            data-testid="create-tr-icons-wrapper"
-          >
-            <ArrowIcon
-              className="create-tr-icon-arrow-reverse"
-              data-testid="arrow-icon"
-            />
-            <ArrowIcon
-              className="create-tr-icon-arrow"
-              data-testid="arrow-icon"
-            />
-          </div>
-        )}
+
+        <button
+          className="create-tr-switch-button"
+          data-testid="create-tr-switch-button"
+          onClick={changeButtonClickHandler}
+        >
+          <ArrowIcon
+            className="create-tr-icon-arrow-reverse"
+            data-testid="arrow-icon"
+          />
+          <ArrowIcon
+            className="create-tr-icon-arrow"
+            data-testid="arrow-icon"
+          />
+          <span className="current-value-type">{currentValueType}</span>
+        </button>
       </div>
-      {buttonTitle && (
-        <Button
-          extraStyleClasses={buttonExtraClasses}
-          onClickHandle={buttonClickHandler}
-        >
-          {buttonTitle}
-        </Button>
-      )}
-      {bottomText && (
-        <p
-          className="create-tr-note"
-          data-testid="create-tr-bottom-note"
-        >
-          {bottomText}
-        </p>
-      )}
+
+      <Button
+        extraStyleClasses={buttonExtraClasses}
+        onClickHandle={actionButtonClickHandler}
+      >
+        {buttonTitle}
+      </Button>
+
+      <p
+        className="create-tr-note"
+        data-testid="create-tr-bottom-note"
+      >
+        {finaBottomValue}
+      </p>
     </div>
   )
 }
 
-export default CreateTransactionFiled
+export default CreateTransactionField
--- src/components/containers/CreateTransaction/createTransactionField.test.js
+++ src/components/containers/CreateTransaction/createTransactionField.test.js
@@ -4,56 +4,97 @@ import CreateTransactionField from './createTransactionField'
 
 const ONCHANGEHANDLESAMPLE = () => {}
 
+const TRANSACTIONDATASAMPLE = {
+  fiatName: 'USD',
+  tokenName: 'BTC',
+  exchangeRate: 22343.23,
+  maxValueInToken: 450,
+}
+
 const PROPSSAMPLE = {
   label: 'Label',
   buttonTitle: 'Button title',
   placeholder: 'Placeholder',
-  buttonClickHandler: () => {},
-  withIcon: true,
-  bottomText: 'Bottom text',
   value: '',
+  transactionData: TRANSACTIONDATASAMPLE,
   setValue: ONCHANGEHANDLESAMPLE,
 }
 
 test('Render TextField component', () => {
-  const mockHandleClickFn = jest.fn()
+  const mockOnCHangeFn = jest.fn()
   render(
     <CreateTransactionField
       label={PROPSSAMPLE.label}
       buttonTitle={PROPSSAMPLE.buttonTitle}
       placeholder={PROPSSAMPLE.placeholder}
-      buttonClickHandler={PROPSSAMPLE.buttonClickHandler}
-      withIcon
-      bottomText={PROPSSAMPLE.bottomText}
-      value={PROPSSAMPLE.value}
-      setValue={mockHandleClickFn}
+      inputValue={PROPSSAMPLE.value}
+      setInputValue={mockOnCHangeFn}
+      transactionData={PROPSSAMPLE.transactionData}
     />,
   )
 
   const component = screen.getByTestId('create-transaction-field')
   const label = screen.getByTestId('create-transaction-label')
   const input = screen.getByTestId('input')
-  const iconsWrapper = screen.getByTestId('create-tr-icons-wrapper')
+  const switchButton = screen.getByTestId('create-tr-switch-button')
   const arrowIcons = screen.getAllByTestId('arrow-icon')
   const actionButton = screen.getByTestId('button')
   const bottomNote = screen.getByTestId('create-tr-bottom-note')
 
   expect(component).toBeInTheDocument()
-  expect(component).toHaveClass('create-tr-field-bottom')
 
   expect(label).toBeInTheDocument()
   expect(label.textContent).toBe(PROPSSAMPLE.label)
 
   expect(input).toBeInTheDocument()
-  fireEvent.change(input, { target: { value: 'test' } })
-  expect(input).toHaveValue('test')
 
-  expect(iconsWrapper).toBeInTheDocument()
+  fireEvent.change(input, {
+    target: { value: TRANSACTIONDATASAMPLE.maxValueInToken },
+  })
+  expect(input).toHaveValue(TRANSACTIONDATASAMPLE.maxValueInToken.toString())
+  expect(bottomNote).toHaveTextContent(
+    TRANSACTIONDATASAMPLE.maxValueInToken * TRANSACTIONDATASAMPLE.exchangeRate,
+  )
+
+  expect(switchButton).toBeInTheDocument()
   expect(arrowIcons).toHaveLength(2)
 
   expect(actionButton).toBeInTheDocument()
   expect(actionButton).toHaveTextContent(PROPSSAMPLE.buttonTitle)
 
   expect(bottomNote).toBeInTheDocument()
-  expect(bottomNote).toHaveTextContent(PROPSSAMPLE.bottomText)
+})
+
+test('Render TextField component fdf', () => {
+  const mockOnCHangeFn = jest.fn()
+  render(
+    <CreateTransactionField
+      label={PROPSSAMPLE.label}
+      buttonTitle={PROPSSAMPLE.buttonTitle}
+      placeholder={PROPSSAMPLE.placeholder}
+      inputValue={PROPSSAMPLE.value}
+      setInputValue={mockOnCHangeFn}
+      transactionData={PROPSSAMPLE.transactionData}
+    />,
+  )
+
+  // const switchButton = screen.getByTestId('create-tr-switch-button')
+  // const actionButton = screen.getByTestId('button')
+  // const bottomNote = screen.getByTestId('create-tr-bottom-note')
+  // const input = screen.getByTestId('input')
+
+  // fireEvent.click(actionButton)
+  // expect(input).toHaveValue(TRANSACTIONDATASAMPLE.maxValueInToken)
+  // fireEvent.click(switchButton)
+  // expect(bottomNote).toHaveTextContent(
+  //   TRANSACTIONDATASAMPLE.maxValueInToken
+  // )
+})
+
+test('Render TextField component without transactionData', () => {
+  render(<CreateTransactionField />)
+
+  expect(
+    screen.queryByTestId('create-transaction-field'),
+  ).not.toBeInTheDocument()
 })
