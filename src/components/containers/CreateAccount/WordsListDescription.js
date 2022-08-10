89e22b476acdf2676c3c1bd3671ec8ef56556ed7
Renie Siqueira <renie.webdev@gmail.com>
2022-08-10 18:49:48 +0300

    feat: adding break line after periods.

--- src/components/containers/CreateAccount/WordsListDescription.js
+++ src/components/containers/CreateAccount/WordsListDescription.js
@@ -9,14 +9,15 @@ const WordsDescription = () => {
         className="center-text words-list-description"
         data-testid="description-paragraph"
       >
-        Write down all of the next words. They will be asked to restores your
-        wallet in the future.
+        Write down all of the next words.
+        <br /> They will be asked to restores your wallet in the future.
       </p>
       <p
         className="center-text words-list-description"
         data-testid="description-paragraph"
       >
-        Save they in a very safe place. It is only your backup
+        Save they in a very safe place.
+        <br /> It is only your backup
       </p>
     </CenteredLayout>
   )
