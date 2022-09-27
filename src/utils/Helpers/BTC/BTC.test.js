774aa9d9c55e03b2156aaebbd825bef0789961e9
owlsua <allatsnow@gmail.com>
2022-09-27 14:02:18 +0200

    add tests for convertBtcToSatoshi function

--- src/utils/Helpers/BTC/BTC.test.js
+++ src/utils/Helpers/BTC/BTC.test.js
@@ -4,6 +4,7 @@ import {
   getParsedTransactions,
   getConfirmationsAmount,
   parseFeesEstimates,
+  convertBtcToSatoshi,
 } from './BTC'
 
 import { fees, rawTransactions, utxos } from '@TestData'
@@ -55,6 +56,56 @@ test('Satoshi to BTC', () => {
   expect(resultBalance1).toBe(balance1)
   expect(resultBalance2).toBe(balance2)
   expect(resultBalance3).toBe(balance3)
+})
+
+test('BTC to Satoshi', () => {
+  const BTC1 = 0.00001
+  const BTC2 = 0.00002
+  const BTC3 = 0.00003
+  const BTC4 = 0.00004
+  const BTC5 = 0.00005
+  const BTC6 = 0.00006
+  const BTC7 = 0.00007
+  const BTC8 = 0.00008
+  const BTC9 = 0.00009
+  const BTC10 = 1
+
+  const satoshi1 = 1000
+  const satoshi2 = 2000
+  const satoshi3 = 3000
+  const satoshi4 = 4000
+  const satoshi5 = 5000
+  const satoshi6 = 6000
+  const satoshi7 = 7000
+  const satoshi8 = 8000
+  const satoshi9 = 9000
+  const satoshi10 = 100000000
+
+  const resultBalance1 = convertBtcToSatoshi(BTC1)
+  const resultBalance2 = convertBtcToSatoshi(BTC2)
+  const resultBalance3 = convertBtcToSatoshi(BTC3)
+  const resultBalance4 = convertBtcToSatoshi(BTC4)
+  const resultBalance5 = convertBtcToSatoshi(BTC5)
+  const resultBalance6 = convertBtcToSatoshi(BTC6)
+  const resultBalance7 = convertBtcToSatoshi(BTC7)
+  const resultBalance8 = convertBtcToSatoshi(BTC8)
+  const resultBalance9 = convertBtcToSatoshi(BTC9)
+  const resultBalance10 = convertBtcToSatoshi(BTC10)
+
+  expect(resultBalance1).toBe(satoshi1)
+  expect(resultBalance2).toBe(satoshi2)
+  expect(resultBalance3).toBe(satoshi3)
+  expect(resultBalance4).toBe(satoshi4)
+  expect(resultBalance5).toBe(satoshi5)
+  expect(resultBalance6).toBe(satoshi6)
+  expect(resultBalance7).toBe(satoshi7)
+  expect(resultBalance8).toBe(satoshi8)
+  expect(resultBalance9).toBe(satoshi9)
+  expect(resultBalance10).toBe(satoshi10)
+
+  expect(resultBalance1).not.toBe(0.00001 * 100000000)
+  expect(resultBalance2).not.toBe(0.00002 * 100000000)
+  expect(resultBalance7).not.toBe(0.00007 * 100000000)
 })
 
 test('Transactions parse', async () => {
