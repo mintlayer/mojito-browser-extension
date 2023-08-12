04ba37bef1c8da1d28260f9348a6cc8bc77390a9
owlsua <allatsnow@gmail.com>
2023-08-12 12:41:17 +0200

    feat: update Dashboard

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,13 +1,15 @@
 import useStyleClasses from './UseStyleClasses/useStyleClasses'
 import { useOnClickOutside } from './UseOnClickOutside/useOnClickOutside'
-import useWalletInfo from './UseWalletInfo/useWalletInfo'
+import useBtcWalletInfo from './UseWalletInfo/useBtcWalletInfo'
+import useMlWalletInfo from './UseWalletInfo/useMlWalletInfo'
 import useExchangeRates from './UseExchangeRates/useExchangeRates'
 import useOneDayAgoExchangeRates from './UseOneDayAgoExchangeRates/useOneDayAgoExchangeRates'
 
 export {
   useStyleClasses,
   useOnClickOutside,
-  useWalletInfo,
+  useBtcWalletInfo,
+  useMlWalletInfo,
   useExchangeRates,
   useOneDayAgoExchangeRates,
 }
