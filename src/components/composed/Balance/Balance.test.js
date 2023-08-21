c218ac50f65efd10760b939012dddaf383b5f19d
owlsua <allatsnow@gmail.com>
2023-08-21 11:10:34 +0200

    test: update unit tests

--- src/components/composed/Balance/Balance.test.js
+++ src/components/composed/Balance/Balance.test.js
@@ -1,18 +1,20 @@
 import { render, screen } from '@testing-library/react'
 import Balance from './Balance'
-import { AccountProvider } from '@Contexts'
+import { AccountProvider, SettingsProvider } from '@Contexts'
 
-const BALANCESAMPLE = 1
-const exchangeRate = 25000
+const BALANCE_SAMPLE = 1
+const EXCHANGE_RATE_SAMPLE = 25000
 
 test('Render account balance with ML', () => {
   render(
     <AccountProvider value={{ walletType: { name: 'Mintlayer' } }}>
-      <Balance
-        balance={BALANCESAMPLE}
-        exchangeRate={exchangeRate}
-      />
-      ,
+      <SettingsProvider>
+        <Balance
+          balance={BALANCE_SAMPLE}
+          exchangeRate={EXCHANGE_RATE_SAMPLE}
+        />
+        ,
+      </SettingsProvider>
     </AccountProvider>,
   )
   const currantBalanceComponent = screen.getByTestId('current-balance')
@@ -21,20 +23,22 @@ test('Render account balance with ML', () => {
   expect(balanceParagraphs).toHaveLength(2)
   expect(currantBalanceComponent).toBeInTheDocument()
 
-  expect(balanceParagraphs[0].textContent).toBe(BALANCESAMPLE + ' ML')
+  expect(balanceParagraphs[0].textContent).toBe(BALANCE_SAMPLE + ' ML')
   expect(balanceParagraphs[1].textContent).toBe(
-    BALANCESAMPLE * exchangeRate + ',00 USD',
+    BALANCE_SAMPLE * EXCHANGE_RATE_SAMPLE + ',00 USD',
   )
 })
 
 test('Render account balance with BTC', () => {
   render(
     <AccountProvider value={{ walletType: { name: 'Bitcoin' } }}>
-      <Balance
-        balance={BALANCESAMPLE}
-        exchangeRate={exchangeRate}
-      />
-      ,
+      <SettingsProvider>
+        <Balance
+          balance={BALANCE_SAMPLE}
+          exchangeRate={EXCHANGE_RATE_SAMPLE}
+        />
+        ,
+      </SettingsProvider>
     </AccountProvider>,
   )
   const currantBalanceComponent = screen.getByTestId('current-balance')
@@ -43,8 +47,31 @@ test('Render account balance with BTC', () => {
   expect(balanceParagraphs).toHaveLength(2)
   expect(currantBalanceComponent).toBeInTheDocument()
 
-  expect(balanceParagraphs[0].textContent).toBe(BALANCESAMPLE + ' BTC')
+  expect(balanceParagraphs[0].textContent).toBe(BALANCE_SAMPLE + ' BTC')
   expect(balanceParagraphs[1].textContent).toBe(
-    BALANCESAMPLE * exchangeRate + ',00 USD',
+    BALANCE_SAMPLE * EXCHANGE_RATE_SAMPLE + ',00 USD',
   )
+})
+
+test('renders balance with zero value when networkType is testnet', () => {
+  render(
+    <AccountProvider value={{ walletType: { name: 'Bitcoin' } }}>
+      <SettingsProvider value={{ networkType: 'testnet' }}>
+        <Balance
+          balance={BALANCE_SAMPLE}
+          exchangeRate={EXCHANGE_RATE_SAMPLE}
+        />
+        ,
+      </SettingsProvider>
+    </AccountProvider>,
+  )
+
+  const currantBalanceComponent = screen.getByTestId('current-balance')
+  const balanceParagraphs = screen.getAllByTestId('balance-paragraph')
+
+  expect(balanceParagraphs).toHaveLength(2)
+  expect(currantBalanceComponent).toBeInTheDocument()
+
+  expect(balanceParagraphs[0].textContent).toBe(BALANCE_SAMPLE + ' BTC')
+  expect(balanceParagraphs[1].textContent).toBe('0,00 USD')
 })
