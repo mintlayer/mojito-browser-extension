22d3e7ea6f4e173024497309bddcd23657b8d2e7
owlsua <allatsnow@gmail.com>
2023-08-24 14:10:32 +0200

    feat: add Mintlayer API

--- src/services/API/index.js
+++ src/services/API/index.js
@@ -1,4 +1,5 @@
 import * as Electrum from './Electrum/Electrum'
+import * as Mintlayer from './Mintlayer/Mintlayer'
 import * as ExchangeRates from './ExchangeRates/ExchangeRates'
 
-export { Electrum, ExchangeRates }
+export { Electrum, Mintlayer, ExchangeRates }
