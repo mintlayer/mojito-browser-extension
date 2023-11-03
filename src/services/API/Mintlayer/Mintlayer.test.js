8474c0bc7709d607ab6cb432cf4717c874ee1bf3
owlsua <allatsnow@gmail.com>
2023-11-03 13:39:27 +0100

    test: add unit tests for the new API data

--- src/services/API/Mintlayer/Mintlayer.test.js
+++ src/services/API/Mintlayer/Mintlayer.test.js
@@ -1,15 +1,19 @@
 import {
-  getLastBlockHash,
-  getTransactionData,
-  getTransactionStatus,
-  getAddressTransactions,
-  getAddress,
-  getAddressUtxo,
+  getAddressData,
+  getAddressBalance,
   requestMintlayer,
-  getLastBlockHeight,
-  getFeesEstimates,
-  broadcastTransaction,
 } from './Mintlayer.js'
+
+import { AppInfo } from '@Constants'
+
+import { localStorageMock } from 'src/tests/mock/localStorage/localStorage.js'
+
+import { LocalStorageService } from '@Storage'
+
+Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+const mockId = 'networkType'
+const mockValue = 'testnet'
+LocalStorageService.setItem(mockId, mockValue)
 
 jest.spyOn(console, 'warn').mockImplementation(() => {
   console.warn.restoreMock()
@@ -17,7 +21,7 @@ jest.spyOn(console, 'warn').mockImplementation(() => {
 
 jest.useRealTimers()
 
-test('Electrum request', async () => {
+test('Mintlayer API request', async () => {
   jest.spyOn(console, 'error').mockImplementation((err) => {
     expect(err).toBeInstanceOf(Error)
     console.error.restoreMock()
@@ -38,7 +42,7 @@ test('Electrum request', async () => {
   )
 })
 
-test('Electrum resquest - not ok', async () => {
+test('Mintlayer API request - not ok', async () => {
   const fetchAddr = 'testFetch'
   const mockFetch = jest.fn((addr) => {
     return new Promise((resolve, reject) =>
@@ -52,59 +56,20 @@ test('Electrum resquest - not ok', async () => {
     await requestMintlayer(fetchAddr, null, mockFetch)
   }).rejects.toThrowError()
 })
+const TESTNET_WALLET = 'tmt1q996a4kpq2ds4snvvgszud6xfhlkcxj7xysjtamj'
 
-const FIRST_TX_ID =
-  'f0315ffc38709d70ad5647e22048358dd3745f3ce3874223c80a7c92fab0c8ba'
-const FIRST_TESTNET_WALLET = 'n3GNqMveyvaPvUbH469vDRadqpJMPc84JA'
-const BLOCK_HASH_LENGTH = 64
-
-test('Electrum request - getLastBlockHash', async () => {
-  await expect(getLastBlockHash()).resolves.toHaveLength(BLOCK_HASH_LENGTH)
-})
-
-test('Electrum request - getLastBlockHeight', async () => {
-  await expect(getLastBlockHeight()).resolves.not.toBeFalsy()
-})
-
-test('Electrum request - getAdressTransactions', async () => {
-  const result = await getAddressTransactions(FIRST_TESTNET_WALLET)
-  const transactions = JSON.parse(result)
-  expect(transactions.length).toBeGreaterThan(0)
-})
-
-test('Electrum request - getAddress', async () => {
-  const result = await getAddress(FIRST_TESTNET_WALLET)
-  const address = JSON.parse(result)
-  expect(address.address).toBe(FIRST_TESTNET_WALLET)
-})
-
-test('Electrum request - getTransactionStatus', async () => {
-  const result = await getTransactionStatus(FIRST_TX_ID)
-  const status = JSON.parse(result)
-  expect(status.confirmed).toBeTruthy()
-})
-
-test('Electrum request - getTransactionData', async () => {
-  const result = await getTransactionData(FIRST_TX_ID)
+test('getAddressData', async () => {
+  const result = await getAddressData(TESTNET_WALLET)
   const data = JSON.parse(result)
-  expect(data.status.block_height).toBe(1)
+  expect(data.coin_balance).toBeGreaterThan(0)
 })
 
-test('Electrum request - getAdressUtxo', async () => {
-  const result = await getAddressUtxo(FIRST_TESTNET_WALLET)
-  const transactions = JSON.parse(result)
-  expect(transactions.length).toBeGreaterThan(0)
+test('Mintlayer API request - getAddressBalance', async () => {
+  const result = await getAddressBalance(TESTNET_WALLET)
+  expect(result.balanceInTokens).toBeGreaterThan(0)
+  expect(result.balanceInCoins).toBeGreaterThan(0)
+  expect(result.balanceInTokens).toBe(
+    result.balanceInCoins / AppInfo.ML_COINS_PER_TOKEN,
+  )
 })
 
-test('Electrum request - getFeesEstimates', async () => {
-  const result = await getFeesEstimates()
-  const fees = JSON.parse(result)
-  expect(Object.keys(fees).length).toBe(28)
-})
-
-test('Electrum request - broadcastTransaction', async () => {
-  await expect(
-    async () => await broadcastTransaction({}),
-  ).rejects.toThrowError()
-  expect(console.warn).toHaveBeenCalled()
-})
