475fe8955e50992eefa1cde6797ce9bcf63cc6a5
owlsua <allatsnow@gmail.com>
2023-11-09 17:08:26 +0100

    test: BTC transaction tests fix

--- src/services/Crypto/BTC/BTCTransaction.test.js
+++ src/services/Crypto/BTC/BTCTransaction.test.js
@@ -13,8 +13,8 @@ const mockValue = 'testnet'
 LocalStorageService.setItem(mockId, mockValue)
 
 test('Build transaction', async () => {
-  const from = 'mgLB5u6BG5YTDVkPPjs1rWnZdtb33aDVMT'
-  const to = 'mmLMRUn75mM2FC11ETfsZTtsTDUWSNa9q2'
+  const from = 'tb1qcwuha0qk0fyjdqn74x3gf3ytwsw8net52j0zt6'
+  const to = 'tb1q0n9phs2dqy3fp20w2k990ck6v9syzr4n57k94s'
   const amount = 196168
   const feePerBytes = 5
   const fee =
@@ -22,16 +22,17 @@ test('Build transaction', async () => {
       BTCTransaction.SIZE_CONSTANTS.output * 2 +
       BTCTransaction.SIZE_CONSTANTS.input * 2) *
     feePerBytes
+  const wif = 'cVbL6DU4SkUffGYshdcvz7AEDzUYiy7McLY4VvSD4XLKEF87EEXj'
   const [result] = await buildTransaction({
-    from,
+    to,
     amount,
     fee,
-    to,
-    wif: 'cTTMFqx7bJSB58TSwXzjSKZaM8frxWZjVzzQfCKCFQaswdzDTFKs',
+    wif,
+    from,
   })
 
   const utxos = JSON.parse(await Electrum.getAddressUtxo(from))
-  const selected = CoinSelectionAlgo.utxoSelect(utxos, amount + fee)
+  const selected = CoinSelectionAlgo.utxoSelect(utxos, amount, fee)
   const amountUsed = selected.reduce((acc, utxo) => acc + utxo.value, 0)
 
   expect(result.outs.length).toBe(2)
