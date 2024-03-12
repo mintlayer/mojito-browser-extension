927f51fb7d153ec47a91f27cb3e0b963ad427197
owlsua <allatsnow@gmail.com>
2024-03-12 14:43:52 +0100

    feat: upate address field

--- src/components/containers/SendTransaction/AddressField.js
+++ src/components/containers/SendTransaction/AddressField.js
@@ -8,10 +8,19 @@ import { ML } from '@Helpers'
 
 import './errorMessages.css'
 
-import { AccountContext, SettingsContext } from '@Contexts'
+import { AccountContext, SettingsContext, TransactionContext } from '@Contexts'
 
 const AddressField = ({ addressChanged, errorMessage, setAddressValidity }) => {
   const { addresses, walletType } = useContext(AccountContext)
+  const { transactionMode, currentDelegationInfo } =
+    useContext(TransactionContext)
+  const inputValue =
+    walletType.name === 'Mintlayer' &&
+    transactionMode === AppInfo.ML_TRANSACTION_MODES.STAKING
+      ? currentDelegationInfo.delegation_id
+      : transactionMode === AppInfo.ML_TRANSACTION_MODES.WITHDRAW
+      ? currentDelegationInfo.spend_destination
+      : ''
   const { networkType } = useContext(SettingsContext)
   const currentBtcAddress =
     networkType === AppInfo.NETWORK_TYPES.MAINNET
@@ -29,20 +38,53 @@ const AddressField = ({ addressChanged, errorMessage, setAddressValidity }) => {
     networkType === AppInfo.NETWORK_TYPES.MAINNET
       ? 'bc1... or 1... or 3...'
       : 'tb1... or 1... or 3...'
+  const mintlayerPoolIdPlaceholder =
+    networkType === AppInfo.NETWORK_TYPES.MAINNET ? 'mpool...' : 'tpool...'
+
+  const mintlayerDelegIdPlaceholder =
+    networkType === AppInfo.NETWORK_TYPES.MAINNET ? 'mdelg...' : 'tdelg...'
+
   const placeholder =
-    walletType.name === 'Mintlayer'
+    walletType.name === 'Mintlayer' &&
+    transactionMode === AppInfo.ML_TRANSACTION_MODES.DELEGATION
+      ? mintlayerPoolIdPlaceholder
+      : walletType.name === 'Mintlayer' &&
+        transactionMode === AppInfo.ML_TRANSACTION_MODES.STAKING
+      ? mintlayerDelegIdPlaceholder
+      : walletType.name === 'Mintlayer'
       ? mintlayerAddressPlaceholder
       : bitcoinAddressPlaceholder
 
   const addressErrorMessage =
-    walletType.name === 'Mintlayer'
+    walletType.name === 'Mintlayer' &&
+    transactionMode === AppInfo.ML_TRANSACTION_MODES.DELEGATION
+      ? 'This is not a valid ML pool id.'
+      : walletType.name === 'Mintlayer' &&
+        transactionMode === AppInfo.ML_TRANSACTION_MODES.STAKING
+      ? 'This is not a valid ML delegation id.'
+      : walletType.name === 'Mintlayer'
       ? 'This is not a valid ML address.'
       : 'This is not a valid BTC address.'
 
+  const label =
+    walletType.name === 'Mintlayer' &&
+    transactionMode === AppInfo.ML_TRANSACTION_MODES.DELEGATION
+      ? 'Pool id:'
+      : walletType.name === 'Mintlayer' &&
+        transactionMode === AppInfo.ML_TRANSACTION_MODES.STAKING
+      ? 'Deleg id:'
+      : 'Send to:'
+
   const changeHandle = (ev) => {
-    const value = ev.target.value
+    const value = ev.target.value || inputValue
     const validity =
-      walletType.name === 'Mintlayer'
+      walletType.name === 'Mintlayer' &&
+      transactionMode === AppInfo.ML_TRANSACTION_MODES.DELEGATION
+        ? ML.isMlPoolIdValid(value, networkType)
+        : walletType.name === 'Mintlayer' &&
+          transactionMode === AppInfo.ML_TRANSACTION_MODES.STAKING
+        ? ML.isMlDelegationIdValid(value, networkType)
+        : walletType.name === 'Mintlayer'
         ? ML.isMlAddressValid(value, networkType)
         : validate(value, 'btc', networkType) && value !== currentBtcAddress
     setIsValid(validity)
@@ -56,6 +98,10 @@ const AddressField = ({ addressChanged, errorMessage, setAddressValidity }) => {
     }
     addressChanged && addressChanged(ev)
   }
+  useEffect(() => {
+    inputValue && changeHandle({ target: { value: inputValue } })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputValue])
 
   useEffect(() => {
     setMessage(errorMessage)
@@ -63,7 +109,7 @@ const AddressField = ({ addressChanged, errorMessage, setAddressValidity }) => {
 
   return (
     <TransactionField>
-      <label htmlFor="address">Send to:</label>
+      <label htmlFor="address">{label}</label>
       <Input
         id="address"
         placeholder={placeholder}
@@ -71,6 +117,11 @@ const AddressField = ({ addressChanged, errorMessage, setAddressValidity }) => {
         onChangeHandle={changeHandle}
         setErrorMessage={setMessage}
         validity={isValid}
+        value={inputValue}
+        disabled={
+          walletType.name === 'Mintlayer' &&
+          transactionMode === AppInfo.ML_TRANSACTION_MODES.WITHDRAW
+        }
       />
       <p className="error-message">{message}</p>
     </TransactionField>
