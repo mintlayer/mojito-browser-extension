e3c7791c63cd5a6445f936797fe3d8bc26c6541e
owlsua <allatsnow@gmail.com>
2024-03-15 14:23:57 +0100

    feat: add pool list link

--- src/components/composed/CurrentStaking/CurrentStaking.js
+++ src/components/composed/CurrentStaking/CurrentStaking.js
@@ -6,11 +6,12 @@ import { Wallet } from '@ContainerComponents'
 import { useMlWalletInfo } from '@Hooks'
 import { ML } from '@Helpers'
 
-import { TransactionContext } from '@Contexts'
+import { TransactionContext, SettingsContext } from '@Contexts'
 
 import './CurrentStaking.css'
 
 const CurrentStaking = ({ addressList }) => {
+  const { networkType } = useContext(SettingsContext)
   const { setDelegationStep } = useContext(TransactionContext)
   const { mlDelegationList, mlDelegationsBalance } =
     useMlWalletInfo(addressList)
@@ -20,21 +21,33 @@ const CurrentStaking = ({ addressList }) => {
   }
   const stakingGuideLink =
     'https://mintlayer.info/en/Guides/Staking/browser-extension'
+  const poolListLink =
+    networkType === 'testnet'
+      ? 'https://lovelace.explorer.mintlayer.org/pools'
+      : 'https://explorer.mintlayer.org/pools'
 
   return (
     <VerticalGroup>
       <div className="staking-title-wrapper">
-        <div className="guide-wraper">
-          <h1 className="staking-title">Your current staking</h1>
-          <HelpTooltip
-            message="Staking guide"
-            link={stakingGuideLink}
-          />
-        </div>
+        <div className="main-info">
+          <div className="guide-wraper">
+            <h1 className="staking-title">Your current staking</h1>
+            <HelpTooltip
+              message="Staking guide"
+              link={stakingGuideLink}
+            />
+          </div>
 
-        <p className="total-staked">
-          Total staked: {ML.getAmountInCoins(mlDelegationsBalance)} ML
-        </p>
+          <p className="total-staked">
+            Total staked: {ML.getAmountInCoins(mlDelegationsBalance)} ML
+          </p>
+        </div>
+        <a
+          href={poolListLink}
+          target="_blank"
+        >
+          <Button extraStyleClasses={['pool-button']}>Pool list</Button>
+        </a>
       </div>
 
       <Wallet.DelegationList delegationsList={mlDelegationList} />
