e373fba58f093dfe5a6bd95d57f0c2d3dd1a0af6
owlsua <allatsnow@gmail.com>
2024-03-29 18:36:11 +0100

    feat: add expand button

--- src/components/composed/Header/Header.js
+++ src/components/composed/Header/Header.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
 import { ReactComponent as BackImg } from '@Assets/images/back-button.svg'
 import { ReactComponent as LogoutImg } from '@Assets/images/logout.svg'
+import { ReactComponent as ExpandImg } from '@Assets/images/icon-expand.svg'
 import { ReactComponent as SettingsImg } from '@Assets/images/settings.svg'
 
 import { Button, Logo } from '@BasicComponents'
@@ -40,6 +42,16 @@ const Header = ({ customBackAction, noBackButton = false }) => {
     navigate('/settings')
   }
 
+  const expandHandler = () => {
+    window.open(
+      typeof browser !== 'undefined'
+        ? // eslint-disable-next-line no-undef
+          browser.runtime.getURL('index.html')
+        : chrome.runtime.getURL('index.html'),
+      '_blank',
+    )
+  }
+
   return (
     <header data-testid="header-container">
       {!noBackButton && (
@@ -52,15 +64,36 @@ const Header = ({ customBackAction, noBackButton = false }) => {
         </Button>
       )}
 
-      {unlocked && (
-        <>
+      {!unlocked && (
+        <div className="expand-wrapped">
           <Button
             alternate
             extraStyleClasses={['settings']}
-            onClickHandle={goSettings}
+            onClickHandle={expandHandler}
           >
-            <SettingsImg />
+            <ExpandImg />
           </Button>
+        </div>
+      )}
+
+      {unlocked && (
+        <>
+          <div className="expand-wrapped expand-wrapped-unlocked">
+            <Button
+              alternate
+              extraStyleClasses={['settings']}
+              onClickHandle={expandHandler}
+            >
+              <ExpandImg />
+            </Button>
+            <Button
+              alternate
+              extraStyleClasses={['settings']}
+              onClickHandle={goSettings}
+            >
+              <SettingsImg />
+            </Button>
+          </div>
           <Button
             alternate
             extraStyleClasses={['logout']}
