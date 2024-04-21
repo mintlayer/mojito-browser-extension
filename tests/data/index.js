4b17b2763136aef8b1d4ebc29365ca84a662a227
owlsua <allatsnow@gmail.com>
2024-04-21 11:13:59 +0200

    feat: add test data

---
+++ tests/data/index.js
@@ -0,0 +1,5 @@
+import * as receiverData from './receiver'
+import * as senderData from './sender'
+import * as createRestoreData from './crate-restore'
+
+export { receiverData, senderData, createRestoreData }
