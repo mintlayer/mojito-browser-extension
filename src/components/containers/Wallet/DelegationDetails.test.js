c007bcad99014ab59f4d383ee74d04b7907f9290
Sergey Chystiakov <anyxem@gmail.com>
2024-05-13 18:11:15 +0200

    remove non-used code

--- src/components/containers/Wallet/DelegationDetails.test.js
+++ src/components/containers/Wallet/DelegationDetails.test.js
@@ -8,12 +8,6 @@ import { BrowserRouter as Router } from 'react-router-dom'
 Object.defineProperty(window, 'localStorage', { value: localStorageMock })
 LocalStorageService.setItem('unlockedAccount', { name: 'test' })
 
-const mockedUsedNavigate = jest.fn()
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: () => mockedUsedNavigate,
-}))
-
 describe('DelegationDetailsItem', () => {
   it('renders correctly', () => {
     render(
