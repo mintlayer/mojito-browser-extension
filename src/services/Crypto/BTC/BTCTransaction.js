b44f970d1c04923584791606c73a6001e80113ae
owlsua <allatsnow@gmail.com>
2024-06-18 00:31:21 +0200

    refactoring

--- src/services/Crypto/BTC/BTCTransaction.js
+++ src/services/Crypto/BTC/BTCTransaction.js
@@ -1,6 +1,6 @@
 import ECPairFactory from 'ecpair'
 import * as bitcoin from 'bitcoinjs-lib'
-import * as ecc from 'tiny-secp256k1'
+import * as ecc from '@bitcoin-js/tiny-secp256k1-asmjs'
 
 import { Electrum } from '@APIs'
 import { Concurrency, BTC, BTCTransaction } from '@Helpers'
