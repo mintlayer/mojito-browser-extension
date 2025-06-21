ca1408ee3c3c004b1dcf8ee1a404d56ca34d6ba3
Sergey Chystiakov <anyxem@gmail.com>
2025-06-22 01:50:36 +0200

    adjust unlock page

--- src/components/composed/LockedBalanceList/LockedBalanceList.js
+++ src/components/composed/LockedBalanceList/LockedBalanceList.js
@@ -8,100 +8,51 @@ import './LockedBalanceList.css'
 const LockedBalanceList = () => {
   const { lockedUtxos, transactions, fetchingUtxos, currentHeight } =
     useContext(MintlayerContext)
-  // const [updatedUtxosList, setUpdatedUtxosList] = useState([])
 
-  // const getUpdatedUtxosWithDate = useCallback(
-  //   async (utxos) => {
-  //     setLoading(true)
-  //     const updatedUtxos = await Promise.all(
-  //       utxos.map(async (utxo) => {
-  //         if (utxo.utxo.lock.type === 'ForBlockCount') {
-  //           const initialTransaction = transactions.find(
-  //             (tx) => tx.txid === utxo.outpoint.source_id,
-  //           )
-  //
-  //           const blocksToUnlock = utxo.utxo.lock.content - initialTransaction.confirmations
-  //
-  //           console.log('blocksToUnlock', blocksToUnlock)
-  //
-  //           console.log('initialTransaction', initialTransaction)
-  //           if (initialTransaction && !utxo.utxo.lock.content.unlockBlock) {
-  //             // Calculating the unlock date
-  //             // const calculatedUnlockTimestamp =
-  //             //   addDays(
-  //             //     new Date(initialTransaction.date * 1000),
-  //             //     10,
-  //             //   ).getTime() / 1000
-  //
-  //             const calculatedUnlockTimestamp = 1
-  //
-  //             console.log('initialTransaction', initialTransaction)
-  //             const blockData = 1
-  //
-  //             utxo.utxo.lock.content = {
-  //               lockedFor: utxo.utxo.lock.content,
-  //               timestamp: calculatedUnlockTimestamp,
-  //               unlockBlock: blockData + utxo.utxo.lock.content,
-  //             }
-  //           }
-  //         }
-  //         return utxo
-  //       }),
-  //     )
-  //     setLoading(false)
-  //     return updatedUtxos
-  //   },
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  //   [lockedUtxos, transactions],
-  // )
-  //
-  // useEffect(() => {
-  //   const fetchUpdatedUtxos = async () => {
-  //     const updatedUtxos = await getUpdatedUtxosWithDate(lockedUtxos)
-  //     const sortedUtxos = updatedUtxos.sort(
-  //       (a, b) => a.utxo.lock.content.timestamp - b.utxo.lock.content.timestamp,
-  //     )
-  //     setUpdatedUtxosList(sortedUtxos)
-  //   }
-  //
-  //   fetchUpdatedUtxos()
-  // }, [lockedUtxos, transactions, getUpdatedUtxosWithDate])
+  const updatedUtxosList = lockedUtxos
+    .map((utxo) => {
+      if (utxo.utxo.lock.type === 'ForBlockCount') {
+        const initialTransaction = transactions.find(
+          (tx) => tx.txid === utxo.outpoint.source_id,
+        )
 
-  const updatedUtxosList = lockedUtxos.map((utxo) => {
-    if (utxo.utxo.lock.type === 'ForBlockCount') {
-      const initialTransaction = transactions.find(
-        (tx) => tx.txid === utxo.outpoint.source_id,
-      )
+        const blocksToUnlock =
+          utxo.utxo.lock.content - initialTransaction.confirmations
 
-      const blocksToUnlock = utxo.utxo.lock.content - initialTransaction.confirmations
+        if (blocksToUnlock < 0) {
+          return null
+        }
 
-      if(blocksToUnlock < 0) {
-        return null
+        return {
+          ...utxo,
+          utxo: {
+            ...utxo.utxo,
+            lock: {
+              ...utxo.utxo.lock,
+              content: {
+                ...utxo.utxo.lock.content,
+                blocksToUnlock,
+                unlockHeight:
+                  currentHeight -
+                  initialTransaction.confirmations +
+                  utxo.utxo.lock.content,
+                timestamp:
+                  parseInt(initialTransaction.date) +
+                  utxo.utxo.lock.content * 120,
+              },
+            },
+          },
+        }
       }
 
       return {
         ...utxo,
-        utxo: {
-          ...utxo.utxo,
-          lock: {
-            ...utxo.utxo.lock,
-            content: {
-              ...utxo.utxo.lock.content,
-              blocksToUnlock,
-              unlockHeight: currentHeight - initialTransaction.confirmations + utxo.utxo.lock.content,
-              timestamp: initialTransaction.date + utxo.utxo.lock.content * 120, // Assuming 10 minutes per block
-            },
-          },
-        },
       }
-    }
-
-    return {
-      ...utxo,
-    }
-  }).reduce(
-    (acc, utxo) => (utxo ? [...acc, utxo] : acc), []
-  )
+    })
+    .reduce((acc, utxo) => (utxo ? [...acc, utxo] : acc), [])
+    .sort(
+      (a, b) => a.utxo.lock.content.timestamp - b.utxo.lock.content.timestamp,
+    )
 
   return (
     <>
--- src/components/composed/LockedBalanceList/LockedBalanceListItem.js
+++ src/components/composed/LockedBalanceList/LockedBalanceListItem.js
@@ -5,31 +5,23 @@ import './LockedBalanceListItem.css'
 
 const displayDate = (utxo) => {
   if (utxo.utxo.lock.type === 'ForBlockCount') {
-      return `~ ${format(
-        new Date(utxo.utxo.lock.content.timestamp / 1000),
-        'dd/MM/yyyy HH:mm',
-      )} (Block height: ${utxo.utxo.lock.content.unlockHeight})`
-    } else if (utxo.utxo.lock.type === 'UntilTime') {
-      return format(
-        new Date(utxo.utxo.lock.content.timestamp * 1000),
-        'dd/MM/yyyy HH:mm',
-      )
-    }
+    return `~ ${format(
+      new Date(utxo.utxo.lock.content.timestamp * 1000),
+      'dd/MM/yyyy HH:mm',
+    )} (Block height: ${utxo.utxo.lock.content.unlockHeight})`
+  } else if (utxo.utxo.lock.type === 'UntilTime') {
+    return format(
+      new Date(utxo.utxo.lock.content.timestamp * 1000),
+      'dd/MM/yyyy HH:mm',
+    )
   }
+}
 
 const LockedBalanceListItem = ({ index, utxo }) => {
   return (
-    <tr
-      className="locked-balance-list-item"
-    >
-      <td
-        className="locked-balance-cell"
-      >
-        {displayDate(utxo)}
-      </td>
-      <td
-        className="locked-balance-cell"
-      >
+    <tr className="locked-balance-list-item">
+      <td className="locked-balance-cell">{displayDate(utxo)}</td>
+      <td className="locked-balance-cell">
         {utxo.utxo.value.amount.decimal} ML
       </td>
     </tr>
