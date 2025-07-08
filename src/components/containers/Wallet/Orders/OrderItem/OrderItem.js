b3ebe60cb51d998e000467bbbee9f44170acc0d1
owlsua <allatsnow@gmail.com>
2025-07-08 22:37:40 +0200

    feat: add OrderItem and OrderList

---
+++ src/components/containers/Wallet/Orders/OrderItem/OrderItem.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+import { PopUp } from '@ComposedComponents'
+import { ML } from '@Helpers'
+import { ReactComponent as SwapIcon } from '@Assets/images/icon-swap.svg'
+
+import OrderDetails from '../OrderDetails/OrderDetails'
+
+import './OrderItem.css'
+
+const OrderItem = ({ order }) => {
+  const [detailPopupOpen, setDetailPopupOpen] = useState(false)
+
+  const orderClickHandle = () => {
+    setDetailPopupOpen(true)
+  }
+
+  return (
+    <li
+      className={'transaction'}
+      data-testid="order"
+      onClick={orderClickHandle}
+    >
+      <div
+        className={'transaction-logo-type transaction-logo-out delegation-icon'}
+      >
+        <SwapIcon className={'order-swap-icon'} />
+      </div>
+      <div className="transaction-detail">
+        <div>
+          <p
+            className="transaction-id-info"
+            data-testid="order-id"
+          >
+            {ML.formatAddress(order.order_id)}
+          </p>
+          <div className="transaction-date-amount">
+            <div>
+              <span>{order.ask_balance.decimal} </span>
+              <span>{order.ask_currency.ticker}</span>
+            </div>
+            <SwapIcon className={'balance-swap-icon'} />
+            <div>
+              <span>{order.give_balance.decimal} </span>
+              <span>{order.give_currency.ticker}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {detailPopupOpen && (
+        <PopUp setOpen={setDetailPopupOpen}>
+          <OrderDetails order={order} />
+        </PopUp>
+      )}
+    </li>
+  )
+}
+
+export default OrderItem
---
+++ src/components/containers/Wallet/Orders/OrderList/OrderList.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import { Button } from '@BasicComponents'
+import OrderItem from '../OrderItem/OrderItem'
+import { SkeletonLoader } from '@BasicComponents'
+import './OrderList.css'
+
+const PAGE_SIZE = 10
+
+const OrderList = ({ orderList, ordersLoading }) => {
+  const [visibleCount, setVisibleCount] = useState(PAGE_SIZE)
+  const [showedOrders, setShowedOrders] = useState([])
+
+  useEffect(() => {
+    setShowedOrders(orderList.slice(0, visibleCount))
+  }, [visibleCount, orderList])
+
+  const renderSkeletonLoaders = () =>
+    Array.from({ length: 6 }, (_, i) => <SkeletonLoader key={i} />)
+
+  const renderOrders = () => {
+    if (!orderList || !orderList.length) {
+      return (
+        <li
+          className="empty-list"
+          data-testid="order-empty"
+        >
+          No orders available to swap in this wallet
+        </li>
+      )
+    }
+
+    return showedOrders.map((order, index) => (
+      <OrderItem
+        key={index}
+        order={order}
+      />
+    ))
+  }
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + PAGE_SIZE)
+  }
+
+  return (
+    <>
+      <ul
+        className="order-list"
+        data-testid={'order-list'}
+      >
+        {ordersLoading ? renderSkeletonLoaders() : renderOrders()}
+        {showedOrders.length < orderList.length && (
+          <div className="load-more-button-wrapper">
+            <Button onClickHandle={handleLoadMore}>Load more</Button>
+          </div>
+        )}
+      </ul>
+    </>
+  )
+}
+
+export default OrderList
