77baf6ad9df7d45bc81d0a76746230dabe0e2d24
Sergey Chystiakov <anyxem@gmail.com>
2025-07-16 19:50:25 +0200

    chore: change deps of secp256k1

--- src/services/Crypto/BTC/BTCTransaction.test.js
+++ src/services/Crypto/BTC/BTCTransaction.test.js
@@ -1,6 +1,6 @@
 //
 
-// TODO: Investigate why @bitcoin-js/tiny-secp256k1-asmjs and ECPairFactory are not widely used in the Jest environment, and find a solution to make the tests work.
+// TODO: Investigate why @bitcoinerlab/secp256k1 and ECPairFactory are not widely used in the Jest environment, and find a solution to make the tests work.
 test.skip('Address Generation - know data', () => {
   const address = 'dsadadasdad'
   expect(address).toBe('dsadadasdad')
