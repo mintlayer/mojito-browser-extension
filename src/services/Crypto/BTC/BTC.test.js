f470b915373a5348ddd50d640fbcc3257b0ac15d
Sergey <anyxem@gmail.com>
2025-07-16 20:07:28 +0200

    chore: change deps of secp256k1 (#224)

--- src/services/Crypto/BTC/BTC.test.js
+++ src/services/Crypto/BTC/BTC.test.js
@@ -83,7 +83,79 @@
 //   expect(validateMnemonic(knownMnemonic)).toBeTruthy()
 // })
 
-// TODO: Investigate why @bitcoin-js/tiny-secp256k1-asmjs and ECPairFactory are not widely used in the Jest environment, and find a solution to make the tests work.
+import {
+  generateMnemonic,
+  validateMnemonic,
+  getSeedFromMnemonic,
+  getWordList,
+} from './BTC'
+
+import { localStorageMock } from 'src/tests/mock/localStorage/localStorage'
+import { LocalStorageService } from '@Storage'
+
+// Setup localStorage mock
+Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+LocalStorageService.setItem('networkType', 'testnet')
+
+// Test constants
+const ENTROPY_DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+const KNOWN_MNEMONIC = 'labor plate task sniff blanket rose answer gesture time seek crowd capital'
+const INVALID_MNEMONIC = 'invalid mnemonic phrase that should not validate'
+
+describe('BTC Crypto Functions', () => {
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  test('generateMnemonic should create valid 12-word mnemonic', () => {
+    const mnemonic = generateMnemonic(ENTROPY_DATA)
+    const words = mnemonic.split(' ')
+
+    expect(words).toHaveLength(12)
+    expect(typeof mnemonic).toBe('string')
+    expect(mnemonic.length).toBeGreaterThan(0)
+  })
+
+  test('validateMnemonic should validate correct mnemonic', () => {
+    expect(validateMnemonic(KNOWN_MNEMONIC)).toBe(true)
+  })
+
+  test('validateMnemonic should reject invalid mnemonic', () => {
+    expect(validateMnemonic(INVALID_MNEMONIC)).toBe(false)
+    expect(validateMnemonic('')).toBe(false)
+    expect(validateMnemonic('single')).toBe(false)
+  })
+
+  test('getSeedFromMnemonic should generate seed buffer', () => {
+    const seed = getSeedFromMnemonic(KNOWN_MNEMONIC)
+
+    expect(seed).toBeInstanceOf(Buffer)
+    expect(seed.length).toBe(64) // BIP39 seeds are 64 bytes
+  })
+
+  test('getSeedFromMnemonic should be deterministic', () => {
+    const seed1 = getSeedFromMnemonic(KNOWN_MNEMONIC)
+    const seed2 = getSeedFromMnemonic(KNOWN_MNEMONIC)
+
+    expect(seed1.equals(seed2)).toBe(true)
+  })
+
+  test('getWordList should return BIP39 word list', () => {
+    const wordList = getWordList()
+
+    expect(Array.isArray(wordList)).toBe(true)
+    expect(wordList).toHaveLength(2048) // BIP39 standard word list length
+    expect(wordList).toContain('abandon') // First word in BIP39 list
+    expect(wordList).toContain('zoo') // Last word in BIP39 list
+  })
+
+  test('generated mnemonic should be valid', () => {
+    const mnemonic = generateMnemonic(ENTROPY_DATA)
+    expect(validateMnemonic(mnemonic)).toBe(true)
+  })
+})
+
+// TODO: Investigate why @bitcoinerlab/secp256k1 and ECPairFactory are not widely used in the Jest environment, and find a solution to make the tests work.
 test.skip('Address Generation - know data', () => {
   const address = 'dsadadasdad'
   expect(address).toBe('dsadadasdad')
