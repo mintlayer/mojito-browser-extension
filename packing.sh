d8bc7a7e80301c334686e33a731fa10f5ffc7a11
owlsua <allatsnow@gmail.com>
2025-08-07 19:15:47 +0200

    bump version

--- packing.sh
+++ packing.sh
@@ -5,12 +5,12 @@
 rm -f ext.zip
 rm -f extFF.zip 
 
+# Run the version-manifest script
+node ./src/version/version-manifest.js
+node ./src/version/version-mojito.js
+
 # mode inside build dir
 cd build
-
-# Run the version-manifest script
-node ../src/version/version-manifest.js
-node ../src/version/version-mojito.js
 
 CSPHEADER="<meta http-equiv=\"Content-Security-Policy\" content=\"default-src *;img-src * 'self' data: https:;style-src-elem 'self' https:\/\/fonts.googleapis.com;font-src https:\/\/fonts.gstatic.com;\"\/>"
 sed -i "s/<meta name=\"CSP\"\/>/$CSPHEADER/g" index.html
--- src/version/version-manifest.js
+++ src/version/version-manifest.js
@@ -3,20 +3,22 @@ const fs = require('fs')
 // const prettier = require('prettier')
 
 const rootDir = path.resolve(__dirname, '../../')
-const manifestBuildPath = path.join(rootDir, 'build/manifest.json')
+// const manifestBuildPath = path.join(rootDir, 'build/manifest.json')
 const manifestChromePath = path.join(rootDir, 'public/manifestDefault.json')
 const manifestFirefoxPath = path.join(rootDir, 'public/manifestFirefox.json')
 const pkgPath = path.join(rootDir, 'package.json')
 
+console.log('Updating version in manifest files...')
+
 const manifestChrome = require(manifestChromePath)
 const manifestFirefox = require(manifestFirefoxPath)
-const manifestBuild = require(manifestBuildPath)
+// const manifestBuild = require(manifestBuildPath)
 const pkg = require(pkgPath)
 
 manifestChrome.version = pkg.version
 manifestFirefox.version = pkg.version
-manifestBuild.version = pkg.version
+// manifestBuild.version = pkg.version
 
 fs.writeFileSync(manifestChromePath, JSON.stringify(manifestChrome, null, 2))
 fs.writeFileSync(manifestFirefoxPath, JSON.stringify(manifestFirefox, null, 2))
-fs.writeFileSync(manifestBuildPath, JSON.stringify(manifestBuild, null, 2))
+// fs.writeFileSync(manifestBuildPath, JSON.stringify(manifestBuild, null, 2))
