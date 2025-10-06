066068b6913b9d82f07051ff3699ecf7e9720ee8
owlsua <allatsnow@gmail.com>
2025-10-06 23:53:14 +0200

    feat: replace stake delegation with sdk

--- src/pages/DelegationStake/DelegationStake.js
+++ src/pages/DelegationStake/DelegationStake.js
@@ -1,45 +1,37 @@
-import { useContext, useState } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
-import { SendTransaction } from '@ContainerComponents'
+import { SendMlTransaction } from '@ContainerComponents'
 import { VerticalGroup } from '@LayoutComponents'
 import { useExchangeRates, useMlWalletInfo } from '@Hooks'
-import { AccountContext, SettingsContext, TransactionContext } from '@Contexts'
-import { Account } from '@Entities'
-import { Format } from '@Helpers'
+import {
+  AccountContext,
+  SettingsContext,
+  MintlayerContext,
+  TransactionContext,
+} from '@Contexts'
 import { AppInfo } from '@Constants'
-import { MLTransaction, ML as MLHelpers } from '@Helpers'
-import { ML } from '@Cryptos'
 
 import './DelegationStake.css'
+import { Error } from '@BasicComponents'
+import { Loading } from '@ComposedComponents'
 
 const DelegationStakePage = () => {
   const { delegationId } = useParams()
-
-  // staking only for Mintlayer
   const walletType = {
     name: 'Mintlayer',
     ticker: 'ML',
     chain: 'mintlayer',
   }
-
-  const currentDelegationInfo = {
-    delegation_id: delegationId,
-    balance: 0,
-  }
-
   const transactionMode = AppInfo.ML_TRANSACTION_MODES.STAKING
-
   const { addresses, accountID } = useContext(AccountContext)
+  const { client } = useContext(MintlayerContext)
   const { networkType } = useContext(SettingsContext)
-  const { setFeeLoading, setDelegationStep } = useContext(TransactionContext)
   const currentMlAddresses =
     networkType === AppInfo.NETWORK_TYPES.MAINNET
       ? addresses.mlMainnetAddresses
       : addresses.mlTestnetAddresses
-  const [totalFeeFiat, setTotalFeeFiat] = useState(0)
   const [totalFeeCrypto, setTotalFeeCrypto] = useState(0)
-  const [totalFee, setTotalFee] = useState(0)
   const navigate = useNavigate()
   const tokenName = 'ML'
   const fiatName = 'USD'
@@ -51,18 +43,53 @@ const DelegationStakePage = () => {
     setDelegationStep(1)
     navigate('/wallet/' + walletType.name + '/staking')
   }
+  const { setDelegationStep } = useContext(TransactionContext)
   const [isFormValid, setFormValid] = useState(false)
   const [transactionInformation, setTransactionInformation] = useState(null)
+  const [feeLoading, setFeeLoading] = useState(false)
 
   const { exchangeRate } = useExchangeRates(tokenName, fiatName)
   const {
     balance: mlBalance,
     utxos,
     unusedAddresses,
-    feerate,
+    fetchingBalances,
+    fetchingUtxos,
   } = useMlWalletInfo(currentMlAddresses)
-
   const maxValueToken = mlBalance
+
+  const transaction_conditions =
+    utxos.length > 0 &&
+    mlBalance > 0 &&
+    unusedAddresses.change &&
+    unusedAddresses.receive
+
+  const loading = fetchingBalances || fetchingUtxos
+
+  useEffect(() => {
+    const buildTransaction = async () => {
+      if (
+        transaction_conditions &&
+        delegationId &&
+        transactionInformation?.amount > 0
+      ) {
+        setFeeLoading(true)
+        const transaction = await client.buildDelegationStake({
+          amount: transactionInformation.amount,
+          delegation_id: transactionInformation.to,
+        })
+        setTotalFeeCrypto(transaction.JSONRepresentation.fee.decimal)
+        setFeeLoading(false)
+      }
+    }
+    buildTransaction()
+  }, [
+    transaction_conditions,
+    transactionInformation,
+    client,
+    unusedAddresses,
+    delegationId,
+  ])
 
   if (!accountID) {
     console.log('No account id.')
@@ -70,80 +97,15 @@ const DelegationStakePage = () => {
     return
   }
 
-  const changeAddressesLength = currentMlAddresses.mlChangeAddresses.length
-
-  const calculateMlTotalFee = async (transactionInfo) => {
-    setFeeLoading(true)
-    const address = transactionInfo.to
-    const amountToSend = MLHelpers.getAmountInAtoms(transactionInfo.amount)
-    const unusedChangeAddress = unusedAddresses.change
-    const transactionSize = await MLTransaction.calculateTransactionSizeInBytes(
-      {
-        utxos: utxos,
-        changeAddress: unusedChangeAddress,
-        amountToUse: amountToSend,
-        network: networkType,
-        delegationId: address,
-        approximateFee: 0,
-      },
-    )
-    const fee = Math.ceil(feerate * (transactionSize / 1000))
-
-    const newTransactionSize =
-      await MLTransaction.calculateTransactionSizeInBytes({
-        utxos: utxos,
-        changeAddress: unusedChangeAddress,
-        amountToUse: amountToSend,
-        network: networkType,
-        delegationId: address,
-        approximateFee: fee,
-      })
-    const newFee = Math.ceil(feerate * (newTransactionSize / 1000))
-    const newFeeInCoins = MLHelpers.getAmountInCoins(Number(newFee))
-    setTotalFeeFiat(Format.fiatValue(newFeeInCoins * exchangeRate))
-    setTotalFeeCrypto(newFeeInCoins)
-    setTotalFee(newFee)
-    setFeeLoading(false)
-    return newFeeInCoins
-  }
-
   const createTransaction = async (transactionInfo) => {
-    calculateMlTotalFee(transactionInfo)
     setTransactionInformation(transactionInfo)
   }
 
-  const confirmMlTransaction = async (password) => {
-    const amountToSend = MLHelpers.getAmountInAtoms(
-      transactionInformation.amount,
-    )
-    const { mlPrivKeys } = await Account.unlockAccount(accountID, password)
-    const privKey =
-      networkType === 'mainnet'
-        ? mlPrivKeys.mlMainnetPrivateKey
-        : mlPrivKeys.mlTestnetPrivateKey
-
-    const walletPrivKeys = await ML.getWalletPrivKeysList(
-      privKey,
-      networkType,
-      changeAddressesLength,
-    )
-    const keysList = {
-      ...walletPrivKeys.mlReceivingPrivKeys,
-      ...walletPrivKeys.mlChangePrivKeys,
-    }
-
-    const unusedChangeAddress = unusedAddresses.change
-
-    const result = await MLTransaction.sendTransaction({
-      utxos: utxos,
-      keysList: keysList,
-      changeAddress: unusedChangeAddress,
-      amountToUse: amountToSend,
-      network: networkType,
-      delegationId: transactionInformation.to,
-      adjustedFee: totalFee,
+  const confirmMlTransaction = async () => {
+    const result = await client.delegationStake({
+      amount: transactionInformation.amount,
+      delegation_id: transactionInformation.to,
     })
-
     return result
   }
 
@@ -151,24 +113,31 @@ const DelegationStakePage = () => {
     <>
       <div className="page">
         <VerticalGroup>
-          <SendTransaction
-            totalFeeFiat={totalFeeFiat}
+          {loading ? (
+            <div className="page-loading">
+              <Loading />
+            </div>
+          ) : (
+            <></>
+          )}
+          <SendMlTransaction
             totalFeeCrypto={totalFeeCrypto}
-            setTotalFeeCrypto={setTotalFeeCrypto}
+            feeLoading={feeLoading}
             transactionData={transactionData}
             exchangeRate={exchangeRate}
             maxValueInToken={maxValueToken}
             onSendTransaction={createTransaction}
-            calculateTotalFee={calculateMlTotalFee}
             setFormValidity={setFormValid}
-            isFormValid={isFormValid}
+            isFormValid={transaction_conditions && isFormValid}
             confirmTransaction={confirmMlTransaction}
             goBackToWallet={goBackToWallet}
-            transactionMode={transactionMode}
-            currentDelegationInfo={currentDelegationInfo}
-            walletType={walletType}
             preEnterAddress={delegationId}
+            transactionMode={transactionMode}
+            walletType={walletType}
           />
+          {!transaction_conditions && (
+            <Error error="Insufficient funds for the fee. Please wait for the wallet to sync or add coins to the wallet." />
+          )}
         </VerticalGroup>
       </div>
     </>
