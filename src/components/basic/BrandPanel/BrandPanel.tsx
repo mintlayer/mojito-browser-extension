8dd97b8599f94d5907c56a136490e66856d1cccc
owlsua <allatsnow@gmail.com>
2026-04-27 23:19:53 +0200

    BrandPanel: add new component

---
+++ src/components/basic/BrandPanel/BrandPanel.tsx
@@ -0,0 +1,25 @@
+import { ReactComponent as LogoIcon } from '@Assets/images/logo.svg'
+import { ReactComponent as BackgroundHome } from '@Assets/images/background-home.svg'
+
+import styles from './BrandPanel.module.css'
+
+const BrandPanel = () => {
+  return (
+    <div className={styles.brandPanel}>
+      <BackgroundHome className={styles.brandBg} />
+      <div className={styles.brandContent}>
+        <LogoIcon className={styles.brandLogo} />
+        <div className={styles.brandTitle}>
+          <span>Mojito</span>
+        </div>
+        <p className={styles.brandSubtitle}>
+          Your non-custodial
+          <br />
+          Mintlayer wallet
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default BrandPanel
