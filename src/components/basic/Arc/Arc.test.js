06cf4e74d25af9c5a8ddc3b4d2c2fdc3006d0886
owlsua <allatsnow@gmail.com>
2026-07-27 11:28:32 +0200

    refactor(chart): drop the unused d3 tooltip from Arc
    The tooltip element was hidden by ArcChart.css, so the mouse handlers
    and the body-level node they wrote to had no visible effect.

--- src/components/basic/Arc/Arc.test.js
+++ src/components/basic/Arc/Arc.test.js
@@ -1,15 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import * as d3 from 'd3'
 
-import {
-  createArcGenerator,
-  createPieGenerator,
-  createTooltip,
-  buildArc,
-  mouseMoveHandle,
-  mouseOverHandle,
-  mouseOutHandle,
-} from './Arc'
+import { createArcGenerator, createPieGenerator, buildArc } from './Arc'
 
 const ArcData = [
   { value: 10, asset: 'BTC', color: 'orange' },
@@ -35,61 +27,9 @@ test('CreatePieGenerator function', () => {
   expect(generator.endAngle()()).toBe(0.5 * Math.PI)
 })
 
-test('CreateTooltip function', () => {
-  createTooltip()
-  const tooltipComponent = screen.getByTestId('tooltip-container')
-
-  expect(tooltipComponent).toBeInTheDocument()
-})
-
-test('mouseMoveHandle function', () => {
-  const d3Tooltip = d3
-    .select('body')
-    .append('div')
-    .attr('data-testid', 'mousemove-tooltip-container')
-
-  const tooltipContainer = screen.getByTestId('mousemove-tooltip-container')
-
-  mouseMoveHandle(d3Tooltip, { pageX: 0, pageY: 0 })
-
-  expect(tooltipContainer).toHaveAttribute('style', 'left: 0px; top: -35px;')
-})
-
-test('mouseOverHandle function', () => {
-  const d3Tooltip = d3
-    .select('body')
-    .append('div')
-    .attr('data-testid', 'mouseover-tooltip-container')
-
-  const item = { data: { value: 10, asset: 'BTC' } }
-
-  const tooltipContainer = screen.getByTestId('mouseover-tooltip-container')
-
-  mouseOverHandle(d3Tooltip, null, item, 0)
-
-  expect(tooltipContainer).toHaveTextContent(
-    `${item.data.asset} ${item.data.value}`,
-  )
-  expect(tooltipContainer).toHaveAttribute('data-show', 'true')
-})
-
-test('mouseOutHandle function', () => {
-  const d3Tooltip = d3
-    .select('body')
-    .append('div')
-    .attr('data-testid', 'mouseout-tooltip-container')
-
-  const tooltipContainer = screen.getByTestId('mouseout-tooltip-container')
-
-  mouseOutHandle(d3Tooltip, 0)
-
-  expect(tooltipContainer).toHaveAttribute('data-show', 'false')
-})
-
 test('Render Arc', () => {
   render(
     <div>
-      <span data-testid="tooltip-container"></span>
       <svg>
         <g data-testid="arc-container"></g>
       </svg>
@@ -97,13 +37,11 @@ test('Render Arc', () => {
   )
 
   const arcContainer = screen.getByTestId('arc-container')
-  const tooltipContainer = screen.getAllByTestId('tooltip-container')
 
   buildArc({
     container: arcContainer,
     pathData: d3.pie()(ArcData),
     arcGenerator: d3.arc(),
-    tooltip: tooltipContainer,
   })
 
   ArcData.forEach((item) => {
